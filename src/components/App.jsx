a87bce930f10c1debdb07c4e43252e43eb9e0b9d
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2023-07-01 06:48:03 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,21 +11,19 @@ export const App = () => {
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState('');
   const [images, setImages] = useState([]);
-  const [perPage, setPerPage] = useState(12);
   const [loading, setLoading] = useState(false);
   const [showButton, setShowButton] = useState(false);
   const [isOpenModal, setIsOpenModal] = useState(false);
   
   useEffect(() => {
-   
     if (!query) return;
     setLoading(true)
     try{
-      getImages({ page, query, perPage }).then(response => {
+      getImages({ page, query }).then(response => {
         console.log(response);
         const { hits, totalHits } = response.data;
         setImages(prevImages => [...prevImages, ...hits]);
-    setShowButton(page < Math.ceil(totalHits / perPage));
+    setShowButton(page < Math.ceil(totalHits / 12));
       })
     } catch (error) {
       console.log(error);
@@ -34,32 +32,29 @@ export const App = () => {
         setLoading(false)
     }
     
-},[page, query, perPage])
+},[page, query])
   
   
   const handleSubmitForm = query => {
     setImages([]);
     setPage(1);
     setQuery(query);
-
+setIsOpenModal(false)
   };
 
  const  onClickButton = () => {
     setPage(prevState =>( prevState.page + 1))
   }
   
-   const toggleModal = () => {
-   setIsOpenModal(previsOpenModal => ( !previsOpenModal ))
-
-  }
-
-
+const toggleModal = () => {
+  setIsOpenModal(prevState => !prevState);
+};
     return (
       <>
         <Searchbar onSubmit={handleSubmitForm} />
         <ImageGallery onClick={toggleModal} images={images} />
         {loading && (<Loader />)}
-        {showButton && (<Button onClick={onClickButton} />)}
+        {showButton && (<Button isOpenModal={isOpenModal} onClick={onClickButton} />)}
       
        
       </>
